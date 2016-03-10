db9cd0fe1d0261d538b57ee30cbb4706b9ac2f77
tec446-01 <tec446-01@Deb04>
2016-03-10 15:51:32 -0300

    gg

--- sim.py
+++ sim.py
@@ -1,4 +1,5 @@
 import sys
+from random import *
 
 
 #Tranforma o valor da mantissa em um valor decimal com ponto flutuante ( float )
@@ -13,40 +14,38 @@ def binFracToFloat(M):
 def BinTofloat(bValue):
 	S = int(bValue[0],2)
 	E = int(bValue[1:6],2)
-	M = binFracToFloat(bValue[5:])
+	M = binFracToFloat(bValue[6:])
 	BE = 1 if E > 0 else 0
 	return (-1)**S*((2)**(E-15))*(BE+M)
 
 #Soma 2 binarios(str) no padrao IEEE745 resultando em um float
 def sum(A,B):
+	Af = BinTofloat(A)
+	Bf = BinTofloat(B)
+	print str(Af) + ' + ' + str(Bf)
+	return Af + Bf
 
-
-	Ar = BinTofloat(A)
-	Br = BinTofloat(B)
-
-
-	return Ar + Br
 #Transforma valor um valor em float para um cadeia de bit no padrao IEEE745(str)
 def gg(R):
+	print R
+	M = ""
 	S = 0
 	twoE = 0
+	count = 0
+	new_R = 0.0
+	new_R = R
+
 	if R == 0:
 		twoE = -15
 	if R < 0:
 		S = 1
-		R = R * -1
-	print R
-	M = ""
-	count = 0
-	new_R = 0.0
+		new_R = new_R * -1
 
-	new_R = R
 	while (new_R>=2):
 		new_R = new_R/2
 		twoE = twoE + 1
 
 	while (count < 11):
-		print M
 		if(new_R >= 1):
 			new_R = new_R - 1
 		new_R = new_R * 2
@@ -57,11 +56,27 @@ def gg(R):
 			M = M + "0"
 		count = count + 1
 
-
-	print bin(16)[2:]
-	print twoE
 	if twoE == 0:
-		return str(S) + '0' + bin(twoE + 15)[2:] + M
+		r = str(S) + '0' + bin(twoE + 15)[2:] + M
 	else:
-		return str(S) + bin(twoE + 15)[2:] + M
-print gg(sum(sys.argv[1],sys.argv[2]))
+		r = str(S) + bin(twoE + 15)[2:] + M 
+	return r
+#--------------------------------------------------------
+r = gg(sum(sys.argv[1],sys.argv[2]))
+print r
+print BinTofloat(r)
+
+A = ""
+B = ""
+c = 0
+while(c<16):
+	A = A + str(randint(0, 1))
+	c = c + 1
+c = 0
+while(c<16):
+	B = B + str(randint(0, 1))
+	c = c + 1
+
+r = gg(sum(A,B))
+print r
+print BinTofloat(r)
